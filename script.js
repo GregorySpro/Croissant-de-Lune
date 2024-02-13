886d2ae5fd54e830c4873fb6c961c61e785de7a7
Grégory Sergent <gregorysergentpro@gmail.com>
2024-02-13 23:19:18 +0100

    maj js

--- script.js
+++ script.js
@@ -46,3 +46,37 @@ let Menu = {
 
 
 }
+
+
+function ajouter(){
+    let nom = prompt('Entrez le nom de l\' objet');
+    let prix = prompt('Entrez le prix de l\'objet');
+    let description = prompt('Entrez la description de l\'objet');
+    let type = prompt('Entrez le type de l\'objet (boisson / plat / entrée / dessert)');
+
+    if (type == 'boisson'){
+        let nouvelle_boisson = Menu.ajouterBoisson(nom, prix, type, description);
+        let nouvel_objet = document.createElement('div');
+        nouvel_objet.className = "item";
+        
+
+        let Titre_boisson = document.createElement('h4');
+        Titre_boisson.textContent = Boissons.nomBoisson;
+
+        let Description_boisson = document.createElement('p');
+        Description_boisson.textContent = Boissons.descriptionBoisson;
+
+        let Prix_boisson = document.createElement('h3');
+        Prix_boisson.textContent = Boissons.prixBoisson;
+
+
+        nouvel_objet.appendChild(Titre_boisson);
+        nouvel_objet.appendChild(Prix_boisson);
+        nouvel_objet.appendChild(Description_boisson);
+
+
+        document.getElementById('Entrées').appendChild(nouvel_objet);
+
+        
+    }
+}
