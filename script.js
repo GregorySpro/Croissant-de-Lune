98c2998a5cd8c584e26688d4fa867b706fd74e48
Grégory Sergent <gregorysergentpro@gmail.com>
2024-02-14 08:57:22 +0100

    maj css/js

--- script.js
+++ script.js
@@ -57,7 +57,6 @@ function ajouter(){
     if (type == 'boisson'){
         let nouvelle_boisson = Menu.ajouterBoisson(nom, prix, type, description);
         let nouvel_objet = document.createElement('div');
-        let retour_ligne = document.createElement('br');
         nouvel_objet.className = "item";
         
 
@@ -65,19 +64,30 @@ function ajouter(){
         Titre_boisson.textContent = Boissons[0].nomBoisson;
         Titre_boisson.className = "TitreObjet";
 
+
         let Description_boisson = document.createElement('p');
         Description_boisson.textContent = Boissons[0].descriptionBoisson;
+        let DivDescription = document.createElement('div');
+        DivDescription.className = "DivDescription";
         Description_boisson.className = "DescriptionObjet";
+        DivDescription.appendChild(Description_boisson);
+
 
         let Prix_boisson = document.createElement('h3');
-        Prix_boisson.textContent = Boissons[0].prixBoisson;
+        Prix_boisson.textContent = Boissons[0].prixBoisson + ' €';
         Prix_boisson.className = "PrixObjet";
 
 
+        let Photo_boisson = document.createElement('img');
+        Photo_boisson.src = 'img/Logo.png';
+        Photo_boisson.className = "PhotoObjet";
+
+
         nouvel_objet.appendChild(Titre_boisson);
-        nouvel_objet.appendChild(retour_ligne);
+        nouvel_objet.appendChild(Photo_boisson);
+        nouvel_objet.appendChild(DivDescription);
         nouvel_objet.appendChild(Prix_boisson);
-        nouvel_objet.appendChild(Description_boisson);
+        
 
 
         document.getElementById('Entrées').appendChild(nouvel_objet);
